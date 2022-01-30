92659fa4d27c1ee70a595555a6532972c79207c6
massaaki <eu@massaaki.com>
2022-01-30 18:14:31 -0300

    refactor: remove typeorm

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,9 @@
 import express from "express";
 
-import createConnection from "@/infra/database-adapters/typeorm-adapters/typeorm-configs/typeorm-connection";
 import setupMiddlewares from "./setup/setup-middlewares";
 import setupRoutes from "./setup/setup-routes";
 import setupApolloServer from "./setup/setup-apollo-server";
 
-createConnection();
 const app = express();
 setupMiddlewares(app);
 setupApolloServer(app);
