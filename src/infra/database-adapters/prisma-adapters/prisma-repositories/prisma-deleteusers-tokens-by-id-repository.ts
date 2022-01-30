bf214b0a1547e51fa3354d743e639b3721fdca81
massaaki <eu@massaaki.com>
2022-01-29 22:16:10 -0300

    feat(infra): add delete-by-id

---
+++ src/infra/database-adapters/prisma-adapters/prisma-repositories/prisma-deleteusers-tokens-by-id-repository.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { IDeleteUsersTokensByIdRepository } from "@/application/infra-protocols/db/delete-users-tokens-by-id-repository";
+
+export class PrismaCreateUsersTokensRepository implements IDeleteUsersTokensByIdRepository {
+  async deleteById(id: string): Promise<void> {
+    const client = new PrismaClient();
+
+    await client.usersTokens.delete({
+      where: {
+        id
+      }
+    });
+  }
+
+}
