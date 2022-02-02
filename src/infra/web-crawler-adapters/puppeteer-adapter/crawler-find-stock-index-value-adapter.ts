a1927f00a62e26eeae60fc40dacd749ba26d9ca8
massaaki <eu@massaaki.com>
2022-02-01 23:39:02 -0300

    feat(infra): add web stock market index adapter implementation

---
+++ src/infra/web-crawler-adapters/puppeteer-adapter/crawler-find-stock-index-value-adapter.ts
@@ -0,0 +1,92 @@
+import puppeteer from 'puppeteer';
+
+import { CrawlerFindStockIndexValueRequestDto } from "@/application/dtos/crawler-find-stock-index-value-dto/crawler-find-stock-index-value-request-dto";
+import { CrawlerFindStockIndexValueResponseDto } from "@/application/dtos/crawler-find-stock-index-value-dto/crawler-find-stock-index-value-response-dto";
+import { ICrawlerFindStockIndexValue } from "@/application/infra-protocols/web-crawler/crawler-find-stock-index-value";
+
+
+// Helper
+function convertNumber(value: string) {
+  let result = '';
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] === ',') {
+      // ignore
+    } else if (value[i] === '.') {
+      result += '.'
+    } else {
+      result += value[i];
+    }
+  }
+
+  return parseFloat(result);
+}
+
+
+export class CrawlerFindStockIndexValueAdapter implements ICrawlerFindStockIndexValue {
+  async scrap(request: CrawlerFindStockIndexValueRequestDto): Promise<CrawlerFindStockIndexValueResponseDto> {
+    try {
+      const { code, siteUrl } = request;
+      let codeOnWeb = null;
+
+      switch (code) {
+        case 'IBOV': {
+          codeOnWeb = '%5EBVSP';
+        };
+        default: {
+          codeOnWeb = null;
+        }
+      }
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.goto(`https://finance.yahoo.com/quote/${codeOnWeb}`, {
+        waitUntil: 'networkidle2'
+      });
+
+
+      // select element
+      const result: any = await page.evaluate(() => {
+        const info = Array.from(document.querySelectorAll("#quote-header-info div")).map(x => {
+          return x.textContent
+        });
+
+        // getting informations
+        const marketOpen = !!info[15]?.includes("open");
+        let data;
+        let fluctuationNegative = 1;
+        if (info[15]?.includes("+")) {
+          data = info[15].split(" ")[0].split("+");
+        } else {
+          data = info[15]?.split(" ")[0].split("-");
+          fluctuationNegative = -1;
+        }
+
+        if (data) {
+          const value = data[0];
+          const fluctuation = data[1];
+
+          return {
+            value,
+            fluctuation,
+            fluctuationNegative,
+            marketOpen
+          };
+        }
+        return false;
+      })
+
+      if (!result) {
+        return null;
+      }
+
+      return {
+        value: convertNumber(result.value)
+      }
+
+    } catch {
+      return null;
+    }
+  }
+}
+
