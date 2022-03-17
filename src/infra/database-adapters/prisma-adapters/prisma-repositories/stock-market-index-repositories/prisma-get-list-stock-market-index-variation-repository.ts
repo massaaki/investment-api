42dcadece0883591da4a61fba966dfccb6a96936
massaaki <eu@massaaki.com>
2022-03-17 16:20:53 -0300

    feat: create PrismaStockMarketIndexVariationRepository implementation

---
+++ src/infra/database-adapters/prisma-adapters/prisma-repositories/stock-market-index-repositories/prisma-get-list-stock-market-index-variation-repository.ts
@@ -0,0 +1,27 @@
+import { Client } from '../../client'
+
+import { GetStockMarketIndexVariationRepositoryResponseDto, IGetStockMarketIndexVariationRepository } from "@/application/infra-protocols/db/stock-market-index-daily-variations-repositories/get-list-stock-market-index-variation-repository";
+
+export class PrismaGetListStockMarketIndexVariationRepository implements IGetStockMarketIndexVariationRepository {
+  async getByCode(code: string): Promise<GetStockMarketIndexVariationRepositoryResponseDto> {
+    try {
+      const client = Client.getInstance();
+
+      // creating a currentDate-12months to filter in database request
+      const today = new Date();
+      const totalDays = 30 * 12; // 12 months
+      const baseData = new Date(new Date().setDate(today.getDate() - totalDays))
+      
+      const list = await client.stockMarketIndexDailyVariation.findMany({
+        where: {
+          created_at: {
+            gt: baseData
+          }
+        }
+      })
+      return list;
+    } catch (err) {
+      return null;
+    }
+  }
+}
